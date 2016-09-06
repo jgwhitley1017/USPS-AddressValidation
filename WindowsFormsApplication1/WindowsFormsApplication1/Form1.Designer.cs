7c3566a1ec800756caa8706084088e7f64cee163
jgwhitley1017 <joseph.whitley@outlook.com>
2016-09-06 19:41:25 -0400

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAddress1 = new System.Windows.Forms.TextBox();
+            this.txtAddress2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtZip5 = new System.Windows.Forms.TextBox();
+            this.txtZip4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address 1";
+            // 
+            // txtAddress1
+            // 
+            this.txtAddress1.Location = new System.Drawing.Point(12, 25);
+            this.txtAddress1.Name = "txtAddress1";
+            this.txtAddress1.Size = new System.Drawing.Size(115, 20);
+            this.txtAddress1.TabIndex = 1;
+            // 
+            // txtAddress2
+            // 
+            this.txtAddress2.Location = new System.Drawing.Point(12, 64);
+            this.txtAddress2.Name = "txtAddress2";
+            this.txtAddress2.Size = new System.Drawing.Size(115, 20);
+            this.txtAddress2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(12, 103);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(115, 20);
+            this.txtCity.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "State";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(12, 142);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(115, 20);
+            this.txtState.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Zip 5";
+            // 
+            // txtZip5
+            // 
+            this.txtZip5.Location = new System.Drawing.Point(12, 181);
+            this.txtZip5.Name = "txtZip5";
+            this.txtZip5.Size = new System.Drawing.Size(54, 20);
+            this.txtZip5.TabIndex = 9;
+            // 
+            // txtZip4
+            // 
+            this.txtZip4.Location = new System.Drawing.Point(88, 181);
+            this.txtZip4.Name = "txtZip4";
+            this.txtZip4.Size = new System.Drawing.Size(39, 20);
+            this.txtZip4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "+4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(72, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "-";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(51, 211);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 13;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(138, 242);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtZip4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtZip5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAddress2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAddress1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAddress1;
+        private System.Windows.Forms.TextBox txtAddress2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtZip5;
+        private System.Windows.Forms.TextBox txtZip4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnValidate;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public const string APIUser = "979MULTI5286";
+        public const string USPSURL = "http://production.shippingapis.com/ShippingAPI.dll";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private XDocument Request()
+        {
+            XDocument doc = new XDocument();
+            XElement usps = new XElement("AddressValidateRequest");
+            doc.Add(usps);
+            usps.SetAttributeValue("USERID", APIUser);
+            usps.Add(new XElement("IncludeOptionalElements", true));
+            usps.Add(new XElement("ReturnCarrierRoute", true));
+            XElement adr = new XElement("Address");
+            adr.SetAttributeValue("ID", 0);
+            adr.Add(new XElement("Address1", txtAddress1.Text));
+            adr.Add(new XElement("Address2", txtAddress2.Text));
+            adr.Add(new XElement("City", txtCity.Text));
+            adr.Add(new XElement("State", txtState.Text));
+            adr.Add(new XElement("Zip5", txtZip5.Text));
+            adr.Add(new XElement("Zip4", txtZip4.Text));
+            usps.Add(adr);
+            return doc;
+        }
+
+        private void SendRequest()
+        {
+            WebClient url = new WebClient();
+            url.QueryString.Add("API", "Verify");
+            url.QueryString.Add("XML", Request().ToString());
+            HandleResponse(url.DownloadString(USPSURL));
+        }
+
+        private void HandleResponse(string response)
+        {
+            XDocument doc = XDocument.Parse(response);
+            XElement adr = doc.Element("AddressValidateResponse").Element("Address");
+            if (adr.Element("Error") != null)
+            {
+                MessageBox.Show(adr.Element("Error").Element("Description").Value, "Error Code " + adr.Element("Error").Element("Number").Value);
+            }
+            else
+            {
+                XElement element;
+                txtAddress1.Text = (element = adr.Element("Address2")) != null ? element.Value : "";
+                txtAddress2.Text = (element = adr.Element("Address1")) != null ? element.Value : "";
+                txtCity.Text = (element = adr.Element("City")) != null ? element.Value : "";
+                txtState.Text = (element = adr.Element("State")) != null ? element.Value : "";
+                txtState.Text = (element = adr.Element("Zip")) != null ? element.Value : "";
+            }
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            SendRequest();
+        }
+    }
+}
